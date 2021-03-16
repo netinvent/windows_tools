0b56fb5763ee172895ccfa642241ab6bd3f49562
Orsiris de Jong <ozy@netpower.fr>
2021-03-16 16:09:13 +0100

    Allow product key to be None

--- tests/test_product_key.py
+++ tests/test_product_key.py
@@ -17,7 +17,7 @@ __intname__ = 'tests.windows_tools.product_key'
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2020-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__build__ = '2021021601'
+__build__ = '2021031601'
 
 import re
 
@@ -29,13 +29,15 @@ PRODUCT_KEY_REGEX = r'([A-Z0-9]{5}-){4}[A-Z0-9]{5}'
 def test_get_windows_product_key_from_wmi():
     wmi_key = get_windows_product_key_from_wmi()
     print('Key from WMI: %s' % wmi_key)
-    assert re.match(PRODUCT_KEY_REGEX, wmi_key), 'Found product key matches product key format'
+    if wmi_key is not None:
+        assert re.match(PRODUCT_KEY_REGEX, wmi_key), 'Found product key matches product key format'
 
 
 def test_get_windows_product_key_from_reg():
     reg_key = get_windows_product_key_from_reg()
     print('Key from REG: %s' % reg_key)
-    assert re.match(PRODUCT_KEY_REGEX, reg_key), 'Found product key matches product key format'
+    if reg_key is not None:
+        assert re.match(PRODUCT_KEY_REGEX, reg_key), 'Found product key matches product key format'
 
 
 if __name__ == '__main__':
