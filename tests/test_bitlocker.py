61bb7b8095014a527d723aa6ce0b43c5321fe8e5
Orsiris de Jong <ozy@netpower.fr>
2021-09-21 19:14:22 +0200

    Add logging to tests

--- tests/test_bitlocker.py
+++ tests/test_bitlocker.py
@@ -19,6 +19,7 @@ __copyright__ = 'Copyright (C) 2020-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
 __build__ = '2021021501'
 
+import logging
 from windows_tools.bitlocker import *
 
 
@@ -28,10 +29,13 @@ def test_get_bitlocker_full_status():
     So here we are writing basic tests
     """
     result = get_bitlocker_full_status()
+    print(result)
     assert result['C:']['status'] is not False, 'C: bitlocker status must be None or a string'
     assert result['C:']['protectors'] is not False, 'C: protectors should be None or a string'
 
 
 if __name__ == '__main__':
     print('Example code for %s, %s' % (__intname__, __build__))
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.DEBUG)
     test_get_bitlocker_full_status()
