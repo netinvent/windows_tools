9201c16731476fed1feb7568adc7973e7911cbcb
Orsiris de Jong <ozy@netpower.fr>
2023-05-06 13:59:38 +0200

    bitness: Print current system bitness

--- tests/test_bitness.py
+++ tests/test_bitness.py
@@ -15,9 +15,9 @@ Versioning semantics:
 
 __intname__ = "tests.windows_tools.bitness"
 __author__ = "Orsiris de Jong"
-__copyright__ = "Copyright (C) 2020-2021 Orsiris de Jong"
+__copyright__ = "Copyright (C) 2020-2023 Orsiris de Jong"
 __licence__ = "BSD 3 Clause"
-__build__ = "2021021501"
+__build__ = "2021050601"
 
 from windows_tools.bitness import *
 
@@ -27,7 +27,7 @@ def test_is_64bit():
     Again, we can only assume that all tests are done on 64 bit systems nowadays
     """
     assert is_64bit() is True, "tests should probably be run on a 64 bit system"
-
+    print("Is this a bit system ?", is_64bit())
 
 if __name__ == "__main__":
     print("Example code for %s, %s" % (__intname__, __build__))
