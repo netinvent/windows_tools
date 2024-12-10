25c7c3d2edb03916a138522784ddded9e399a489
deajan <ozy@netpower.fr>
2024-12-10 13:27:50 +0100

    Bump version

--- windows_tools/__init__.py
+++ windows_tools/__init__.py
@@ -15,11 +15,11 @@ Versioning semantics:
 
 __intname__ = "windows_tools"
 __author__ = "Orsiris de Jong"
-__copyright__ = "Copyright (C) 2018-2023 Orsiris de Jong"
+__copyright__ = "Copyright (C) 2018-2024 Orsiris de Jong"
 __description__ = "Toolset for antivirus, NTFS/ReFS ACLs, file ownership, registry, user handling...Well a lot of stuff"
 __licence__ = "BSD 3 Clause"
-__version__ = "2.4.0"
-__build__ = "2023082901"
+__version__ = "2.5.0"
+__build__ = "2024121001"
 
 
 # Make sure we declare this file as namespace holder
