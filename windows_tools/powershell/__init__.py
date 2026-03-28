26db2f916e48928466ca5794c879baeec593e36f
deajan <ozy@netpower.fr>
2026-03-28 19:06:36 +0100

    powershell: Improve powershell elevation options

--- windows_tools/powershell/__init__.py
+++ windows_tools/powershell/__init__.py
@@ -19,8 +19,8 @@ __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2019-2026 Orsiris de Jong"
 __description__ = "PowerShell interpreter wrapper"
 __licence__ = "BSD 3 Clause"
-__version__ = "0.6.0"
-__build__ = "2026032701"
+__version__ = "0.7.0"
+__build__ = "2026032801"
 
 import os
 from logging import getLogger
@@ -34,96 +34,120 @@ import windows_tools.registry
 logger = getLogger()
 
 
-def _powershell_elevator_script(hidden=False):
+def sanitize_filename(file: str) -> str:
+    return "".join(x if x.isalnum() else "_" for x in file)
+
+
+def _powershell_elevator_script(hidden: bool = False, elevate_message: str = None, clear_screen: bool = False, bg_color: str = None) -> str:
     """
     Returns a wrapper script for powershell commands / scripts
     that will try elevation and get stdout/stderr and exit code (0 or 1) of the elevated process
     """
     script = r"""
-    # Run elevated commands and get stdout/stderr and (partial) exitcode
+# Run elevated commands and get stdout/stderr and (partial) exitcode
 
-    try {
-        # Function blatantly stolen from https://stackoverflow.com/a/15669365/2635443
-        function Write-StdErr {
-            param ([PSObject] $InputObject)
-            $outFunc = if ($Host.Name -eq 'ConsoleHost') {
-                [Console]::Error.WriteLine
-            } else {
-            $host.ui.WriteErrorLine
+try {
+    # Function blatantly stolen from https://stackoverflow.com/a/15669365/2635443
+    function Write-StdErr {
+        param ([PSObject] $InputObject)
+        $outFunc = if ($Host.Name -eq 'ConsoleHost') {
+            [Console]::Error.WriteLine
+        } else {
+        $host.ui.WriteErrorLine
+        }
+        if ($InputObject) {
+            [void] $outFunc.Invoke($InputObject.ToString())
+        } else {
+            [string[]] $lines = @()
+            $Input | % { $lines += $_.ToString() }
+            [void] $outFunc.Invoke($lines -join "`r`n")
+        }
+    }
+
+    # Parts blatanly stolen from https://stackoverflow.com/a/60216595/2635443
+
+    # Get the ID and security principal of the current user account
+    $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+    # Get the security principal for the Administrator role
+    $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+    # Check to see if we are currently running "as Administrator"
+    if ($myWindowsPrincipal.IsInRole($adminRole))
+        {
+            #Update window to show we're running as administrator
+            $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+            ___BACKGROUND_COLOR_PLACEHOLDER___
+            ___CLEAR_SCREEN_PLACEHOLDER___
+            ___ELEVATE_MESSAGE_PLACEHOLDER___
+        } else {
+            $capture_stdout = New-TemporaryFile
+            $capture_stderr = New-TemporaryFile
+            # Relaunch our processs as admin
+            # This will disconnect fds, so we need to redirect stdout and stderr in order to catch them
+            $processParams = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+            # Specify the current script path and name as a parameter
+            $processParams.Arguments = $myInvocation.MyCommand.Definition;
+            $processParams.Arguments += " > $capture_stdout 2> $capture_stderr"
+
+            # Indicate that the process should be elevated
+            $processParams.Verb = "RunAs"
+            # We might hide our window, or leave this commented out so options above apply
+            ___HIDDEN_PLACEHOLDER___
+
+            # Start the new process
+            try {
+                $process = [System.Diagnostics.Process]::Start($processParams)
+                While (-not $process.HasExited) {
+                    Start-Sleep -Milliseconds 100
+                }
+                # Arbitrary wait time for fds to close
+                Start-Sleep -Milliseconds 500
+                Write-Host (Get-Content -Path $capture_stdout)
+                Write-StdErr (Get-Content -Path $capture_stderr)
+                # This will only return 0 or 1, other integers are transformed into 1
+                exit($process.ExitCode)
+            } catch {
+                Write-output "Process cannot be launched as admin. Trying as standard user"
             }
-            if ($InputObject) {
-                [void] $outFunc.Invoke($InputObject.ToString())
-            } else {
-                [string[]] $lines = @()
-                $Input | % { $lines += $_.ToString() }
-                [void] $outFunc.Invoke($lines -join "`r`n")
-            }
+        
         }
 
-        # Parts blatanly stolen from https://stackoverflow.com/a/60216595/2635443
-
-        # Get the ID and security principal of the current user account
-        $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-        $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
-
-        # Get the security principal for the Administrator role
-        $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
-
-        # Check to see if we are currently running "as Administrator"
-        if ($myWindowsPrincipal.IsInRole($adminRole))
-            {
-                #Update window to show we're running as administrator
-                $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-                $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-                clear-host
-                $Host.UI.Write("Running elevated NPBackup task processing")
-            } else {
-                $capture_stdout = New-TemporaryFile
-                $capture_stderr = New-TemporaryFile
-                # Relaunch our processs as admin
-                # This will disconnect fds, so we need to redirect stdout and stderr in order to catch them
-                $processParams = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-                # Specify the current script path and name as a parameter
-                $processParams.Arguments = $myInvocation.MyCommand.Definition;
-                $processParams.Arguments += " > $capture_stdout 2> $capture_stderr"
-
-                # Indicate that the process should be elevated
-                $processParams.Verb = "RunAs"
-                # We might hide our window, or leave this commented out so options above apply
-                ___HIDDEN_PLACEHOLDER___
-
-                # Start the new process
-                try {
-                    $process = [System.Diagnostics.Process]::Start($processParams)
-                    While (-not $process.HasExited) {
-                        Start-Sleep -Milliseconds 100
-                    }
-                    # Arbitrary wait time for fds to close
-                    Start-Sleep -Milliseconds 500
-                    Write-Host (Get-Content -Path $capture_stdout)
-                    Write-StdErr (Get-Content -Path $capture_stderr)
-                    # This will only return 0 or 1, other integers are transformed into 1
-                    exit($process.ExitCode)
-                } catch {
-                    Write-output "Process cannot be launched as admin. Trying as standard user"
-                }
-            
-            }
-
-        # The following code will run elevated
-        ___CODE_PLACEHOLDER___
-    } catch {
-        Write-Host "PS Error: $($_.Exception.Message)"*
-        exit(1)
-    }
+    # The following code will run elevated
+    ___CODE_PLACEHOLDER___
+} catch {
+    Write-Host "PS Error: $($_.Exception.Message)"
+    Start-Sleep -Seconds 3
+    exit(1)
+}
     """
     if hidden:
         script = script.replace(
-            "___HIDDEN_PLACEHOLDER___", '$processParams.WindowStyle = "Hidden"'
-        )
+                        "___HIDDEN_PLACEHOLDER___", '$processParams.WindowStyle = "Hidden"'
+                    )
     else:
         script = script.replace("___HIDDEN_PLACEHOLDER___", "")
+    if elevate_message:
+        script = script.replace(
+            "___ELEVATE_MESSAGE_PLACEHOLDER___", '$Host.UI.Write("{}")'.format(elevate_message)
+        )
+    else:
+        script = script.replace("___ELEVATE_MESSAGE_PLACEHOLDER___", "")
+    if bg_color:
+        script = script.replace(
+            "___BACKGROUND_COLOR_PLACEHOLDER___", '$Host.UI.RawUI.BackgroundColor = "{}"'.format(bg_color)
+        )
+    else:
+        script = script.replace("___BACKGROUND_COLOR_PLACEHOLDER___", "")
+    if clear_screen:
+        script = script.replace(
+            "___CLEAR_SCREEN_PLACEHOLDER___", 'Clear-Host'
+        )
+    else:
+        script = script.replace("___CLEAR_SCREEN_PLACEHOLDER___", "")
+
     return script
 
 
@@ -134,7 +158,8 @@ class PowerShellRunner:
 
     def __init__(self, powershell_interpreter=None):
         self._interactive = False
-        self._tmp_script_identifier = __intname__
+        self._identifier_string = __intname__
+        self._elevate_message = "Running elevated powershell command"
 
         self.powershell_interpreter = powershell_interpreter
 
@@ -192,14 +217,25 @@ class PowerShellRunner:
         self._interactive = value
 
     @property
-    def tmp_script_identifier(self):
-        return self._tmp_script_identifier
-
-    @tmp_script_identifier.setter
-    def tmp_script_identifier(self, value):
+    def identifier_string(self):
+        return self._identifier_string
+    
+    @identifier_string.setter
+    def identifier_string(self, value):
         if not isinstance(value, str):
-            raise ValueError("_tmp_script_identifier property must be a string")
-        self._tmp_script_identifier = value
+            raise ValueError("identifier_string property must be a string")
+        self._identifier_string = value
+
+    @property
+    def elevate_message(self):
+        return self._elevate_message
+    
+    @elevate_message.setter
+    def elevate_message(self, value):
+        if not isinstance(value, str):
+            raise ValueError("elevate_message property must be a string")
+        self._elevate_message = value
+
 
     def get_version(self):
         """
@@ -251,6 +287,9 @@ class PowerShellRunner:
         if self.powershell_interpreter is None:
             return False
 
+        if kwargs.pop("elevate", None) is not None:
+            raise EnvironmentError("elevate argument is not supported in run_command method, use run_script with elevate=True instead")
+
         # So older powershell versions used unicode_escape, newer ones are utf-8
         # AFAIK, the change happened between powershell 4 and 5
         if not encoding and self.major_version is not None:
@@ -267,11 +306,12 @@ class PowerShellRunner:
             to_json_suffix = "| ConvertTo-Json -Depth {}".format(json_depth)
 
         # Do not add -NoProfile so we don't end up in a path we're not supposed to
-        command = self.powershell_interpreter + "{} -NoLogo {}{}{}".format(
-            " -NonInteractive" if not self.interactive else "",
-            utf8_prefix,
-            command,
-            to_json_suffix,
+        command = (
+            self.powershell_interpreter
+            + "{} -NoLogo {}{}{}".format(
+                " -NonInteractive" if not self.interactive else "",
+                utf8_prefix, command, to_json_suffix
+            )
         )
         logger.debug("Running powershell command:\n%s", command)
 
@@ -315,19 +355,18 @@ class PowerShellRunner:
                 if script.endswith(".ps1") and os.path.isfile(script):
                     with open(script, "r", encoding=encoding) as fp:
                         script_content = fp.read()
-                    powershell_elevator_script = _powershell_elevator_script(
-                        hidden=not self.interactive
-                    ).replace("___CODE_PLACEHOLDER___", script_content)
+                    powershell_elevator_script = _powershell_elevator_script(hidden=not self.interactive, elevate_message=self.elevate_message).replace(
+                        "___CODE_PLACEHOLDER___", script_content
+                    )
                 else:
                     # Assume we are given an inline script instead of a file
-                    powershell_elevator_script = _powershell_elevator_script(
-                        hidden=not self.interactive
-                    ).replace("___CODE_PLACEHOLDER___", script)
+                    powershell_elevator_script = _powershell_elevator_script(hidden=not self.interactive, elevate_message=self.elevate_message).replace(
+                        "___CODE_PLACEHOLDER___", script
+                    )
 
                 # Write a temporary elevator script with the content of the given script
                 powershell_elevator_temp_script = os.path.join(
-                    tempfile.gettempdir(),
-                    "{}_{}.ps1".format(self._tmp_script_identifier, random_string(8)),
+                    tempfile.gettempdir(), "{}_{}.ps1".format(self.identifier_string, random_string(8))
                 )
                 with open(
                     powershell_elevator_temp_script, "w", encoding=encoding
@@ -342,7 +381,7 @@ class PowerShellRunner:
         # codes 0 or 1 whereas as running with -File argument returns your script exit code
         command = (
             self.powershell_interpreter
-            + ' -executionPolicy Bypass{} -NoLogo -NoProfile -File "{}"'.format(
+            + " -executionPolicy Bypass{} -NoLogo -NoProfile -File \"{}\"".format(
                 " -NonInteractive" if not self.interactive else "", script
             )
             + (" " if len(args) > 0 else " ")
